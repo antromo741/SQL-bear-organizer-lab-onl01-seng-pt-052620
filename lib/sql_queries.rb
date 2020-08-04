30a686d56156616d77f96c4485bcef539e0ffd32
careful-illustrator-5297 <anthonyroma741@gmail.com>
2020-08-04 14:29:44 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -7,15 +7,15 @@ def selects_all_bears_names_and_orders_in_alphabetical_order
 end
 
 def selects_all_bears_names_and_ages_that_are_alive_and_order_youngest_to_oldest
-  "SELECT name, age FROM bears WHERE alive = '1' ORDER BY ASC";
+  "SELECT name, age FROM bears WHERE alive = '1' ORDER BY age ASC";
 end
 
 def selects_oldest_bear_and_returns_name_and_age
-  "SELECT name, age FROM bears ORDER BY age DESC LIMIT !";
+  "SELECT name, age FROM bears ORDER BY age DESC LIMIT 1";
 end
 
 def select_youngest_bear_and_returns_name_and_age
-  "Write your SQL query here"
+  "SELECT name, age FROM bears ORDER BY age ASC limit 1";
 end
 
 def selects_most_prominent_color_and_returns_with_count
