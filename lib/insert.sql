c2bd38cea8c7987396251d5d73ea0a92cc4ec343
careful-illustrator-5297 <anthonyroma741@gmail.com>
2020-08-04 14:23:10 +0000

    Automatically backed up by Learn

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears (id, name, age ,gender,color,temperament,alive) VALUES
+(1, "Mr. Chocolate", 30, "M", "brown","hyper",0),
+(2, "Rowdy", 24, "M", "red","calm",1),
+(3, "Tabitha", 6, "F", "Yellow","Jolly",1),
+(4, "Sargent Brown", 45, "M", "Green","Angry",0),
+(5, "Melissa", 19, "F", "Blue","Sad",1),
+(6, "Grinch", 89, "M", "Greener","Mean",1),
+(7, "Wendy", 28, "F", "Orange","Nice",0),
+(8, null, 10, "M", "Gray","annoying",1);
