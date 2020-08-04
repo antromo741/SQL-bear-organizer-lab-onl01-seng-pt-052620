24bf141f02a38e5343d6e384e563233eb6522191
careful-illustrator-5297 <anthonyroma741@gmail.com>
2020-08-04 14:33:09 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -19,11 +19,11 @@ def select_youngest_bear_and_returns_name_and_age
 end
 
 def selects_most_prominent_color_and_returns_with_count
-  "SELECT MAX(color), COUNT(color) FROM bears GROUP BY color ORDER BY color ASC limit 1";
+  "SELECT MAX(color), COUNT(color) FROM bears GROUP BY color ORDER BY color ASC LIMIT 1";
 end
 
 def counts_number_of_bears_with_goofy_temperaments
-  "Write your SQL query here"
+  "SELECT 
 end
 
 def selects_bear_that_killed_Tim
